6994b7a34dea34f46b6eba691530ec341a88597a
divyam01 <divyam@antino.io>
2021-04-22 12:19:08 +0530

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDWH2nAiSKpgdytQVIuikvxRCax-7md8rA",
+    authDomain: "new-lagroce-app.firebaseapp.com",
+    projectId: "new-lagroce-app",
+    storageBucket: "new-lagroce-app.appspot.com",
+    messagingSenderId: "178860814427",
+    appId: "1:178860814427:web:d9e7adc307c63e3c638f7e"
+  }
 };
