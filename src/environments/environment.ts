6733e104658b54b0aa93690f6f81456f55e40cb4
divyam01 <divyam@antino.io>
2021-04-22 12:13:04 +0530

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig : {
     apiKey: "AIzaSyDWH2nAiSKpgdytQVIuikvxRCax-7md8rA",
     authDomain: "new-lagroce-app.firebaseapp.com",
